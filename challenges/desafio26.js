00cb4ef324b9eaa64979494bd065747076dc0fab
nevesfreire <nando.neves@gmail.com>
2021-03-11 10:35:13 -0300

    27 e 28 faltando

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,4 +1,1 @@
-db.voos.deleteMany({ "empresa.nome": "GOL", "passageiros.pagos": { $in: [5, 6, 7, 8, 9, 10] } }).count();
-
-// Remova todos os voos da empresa GOL em que a quantidade de passageiros pagos esteja entre 5 e 10,
-// incluindo os casos em que a quantidade é 5 e 10. Informe a quantidade de documentos removidos.
+db.voos.deleteMany({ "empresa.nome": "GOL", "passageiros.pagos": { $in: [5, 6, 7, 8, 9, 10] } });
