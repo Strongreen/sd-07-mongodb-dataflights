400df3f8638bb0ad79335407e03b3404d6ba0885
Filipi Firmino <contatoic3web@gmail.com>
2021-03-11 17:14:03 -0300

    challeges 20, 21,22,23 and 24

---
+++ challenges/desafio22.js
@@ -0,0 +1,4 @@
+db.voos.findOne({ $and: [
+  { $or: [{ "empresa.nome": "DELTA AIRLINES" }, { "empresa.nome": "AMERICAN AIRLINES" }] },
+  { $or: [{ "aeroportoOrigem.sigla": "SBGR" }, { "aeroportoDestino.sigla": "KJFK" }] },
+] }, { vooId: 1, _id: 0 });
