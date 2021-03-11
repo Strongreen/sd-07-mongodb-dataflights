f7c9fe89243cf43df2b9278c4eb9a93a9ee2b9b9
Renato Silva <renatosdev@gmail.com>
2021-03-11 16:50:05 -0300

    codeclimate

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,9 +1,9 @@
 db.voos.find(
-    {},
-    {
-      _id: 0,
-      vooId: 1,
-    },
-  )
+  {},
+  {
+    _id: 0,
+    vooId: 1,
+  },
+)
   .skip(9)
   .limit(3);
--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -4,8 +4,8 @@ db.voos.find(
   },
   {
     _id: 0,
-    'empresa.sigla': 1,
-    'empresa.nome': 1,
+    "empresa.sigla": 1,
+    "empresa.nome": 1,
     passageiros: 1,
   },
 );
