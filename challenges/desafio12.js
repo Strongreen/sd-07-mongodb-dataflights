c1ed37ee59a824c2cc35bfe44943692e41a0194b
luc-zago <luc.zago@hotmail.com>
2021-03-11 12:09:56 -0300

    Alterações nos desafios 12 e 13

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -2,7 +2,7 @@ db.getCollection("voos")
   .find({
     $or: [
       { "aeroportoDestino.pais": "BRASIL" },
-      { "aeroportoDestino.pais": "CHILE" },
       { "aeroportoDestino.pais": "ARGENTINA" },
+      { "aeroportoDestino.pais": "CHILE" },
     ],
   });
--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -2,7 +2,7 @@ db.getCollection("voos")
   .find({
     $and: [
       { "aeroportoDestino.continente": { $ne: "EUROPA" } },
-      { "aeroportoDestino.continente": { $ne: "OCEANIA" } },
       { "aeroportoDestino.continente": { $ne: "ÁSIA" } },
+      { "aeroportoDestino.continente": { $ne: "OCEANIA" } },
     ],
   });
