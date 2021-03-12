ce37d48c36ec22cb000e5b2eb952efb533e75352
vitorAmorims <vtamorims@gmail.com>
2021-03-12 06:43:04 -0300

    corrigindo erro de lint do desafio23

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -6,6 +6,6 @@ db.voos
         { litrosCombustivel: { $lt: 1000 } },
       ],
     },
-    { vooId: 1, litrosCombustivel: 1, _id: 0 }
+    { vooId: 1, litrosCombustivel: 1, _id: 0 },
   )
   .limit(1);
