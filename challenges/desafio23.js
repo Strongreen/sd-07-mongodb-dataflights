cfccfe1f19f627be922c7fef19d05d2f5e12d491
jvitorof <j.vitoroflores@gmail.com>
2021-03-12 00:03:27 -0300

    :four_leaf_clover:

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,8 +1,13 @@
 db.getCollection("voos")
   .find(
     { $and: [
-      { litrosCombustivel: { $exist: true } },
+      { litrosCombustivel: { $exists: true } },
       { litrosCombustivel: { $lt: 1000 } },
     ],
-    }, {},
+    },
+    {
+      _id: 0,
+      vooId: 1,      
+      litrosCombustivel: 1
+    },
   ).limit(1);
--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -2,8 +2,14 @@ db.getCollection("voos")
   .find(
     {
       $and: [
-        { litrosCombustivel: { $exist: true } },
+        { litrosCombustivel: { $exists: true } },
         { litrosCombustivel: { $lt: 600 } },
       ],
-    }, {},
+    },
+    {
+      _id: 0,
+      vooId: 1,
+      "empresa.nome": 1,
+      litrosCombustivel: 1
+    },
   ).limit(1);
