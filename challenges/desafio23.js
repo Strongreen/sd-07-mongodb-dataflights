659d3fc04c91b6c36627373efb21a9b4ff3c9972
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-03-12 15:51:40 -0300

    menos de 100 caracteres

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,1 +1,2 @@
-db.voos.findOne({ litrosCombustivel: { $lt: 1000, $exist: true } }, { _id: 0, vooId: 1, litrosCombustivel: 1 });
+db.voos.findOne({ litrosCombustivel: { $lt: 1000, $exist: true } },
+  { _id: 0, vooId: 1, litrosCombustivel: 1 });
