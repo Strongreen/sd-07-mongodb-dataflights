8a8316d19d415f60ff93a8924b967037f45eb8d5
moraisgabri <gabriel8azevedo@gmail.com>
2021-03-15 04:10:10 -0300

    cc

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,8 +1,8 @@
 db.voos.find({
-    $and: [
-      { "aeroportoDestino.continente": { $ne: "EUROPA" } },
-      { "aeroportoDestino.continente": { $ne: "ÁSIA" } },
-      { "aeroportoDestino.continente": { $ne: "OCEANIA" } },
-    ],
+  $and: [
+    { "aeroportoDestino.continente": { $ne: "EUROPA" } },
+    { "aeroportoDestino.continente": { $ne: "ÁSIA" } },
+    { "aeroportoDestino.continente": { $ne: "OCEANIA" } },
+  ],
   },
 ).count();
