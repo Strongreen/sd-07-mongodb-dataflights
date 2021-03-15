74a9e4d5fcd9627bf51a57b30a8bfbb60c6b2150
Zeca Costa <jccostaso@gmail.com>
2021-03-15 16:35:42 -0300

    desafio26.js

--- challenges/desafio25.js
+++ challenges/desafio25.js
@@ -1,4 +1,9 @@
-db.voos.deleteMany({
-  "empresa.nome": "AZUL",
-  litrosCombustivel: { $lt: 400 },
-});
+db.voos.deleteMany(
+  {
+    $and: [
+      { "empresa.nome": "GOL" },
+      { "passageiros.pagos": { $gte: 5 } },
+      { "passageiros.pagos": { $lte: 10 } },
+    ],
+  },
+);
