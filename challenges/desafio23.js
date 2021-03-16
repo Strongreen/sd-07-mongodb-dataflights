4720e9b0bec3e3ab38d555c514b48a8f7ac36169
Bruna Fonsêca <1998bfonseca@gmail.com>
2021-03-15 21:31:51 -0300

    fixing lint issues

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,1 +1,7 @@
-db.voos.findOne({ $and: [{ litrosCombustivel: { $not: { $gt: 1000 } } }, { litrosCombustivel: { $exists: true } }] }, { _id: 0, vooId: 1 });
+db.voos.findOne(
+  {
+    $and:
+    [
+      { litrosCombustivel: { $not: { $gt: 1000 } } },
+      { litrosCombustivel: { $exists: true } }] },
+      { _id: 0, vooId: 1 });
