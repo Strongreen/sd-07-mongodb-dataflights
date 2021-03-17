d1498bfb5234f6abd49d58bc64c827a7c63157e4
Fabio Camargos <fabiocmmcm@gmail.com>
2021-03-17 16:36:49 -0300

    Fixing Lint

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,1 +1,2 @@
-db.voos.findOne({ litrosCombustivel: { $lte: 1000, $exists: 1 } }, { vooId: 1, litrosCombustivel: 1, _id: 0 });
+db.voos.findOne({ litrosCombustivel:
+  { $lte: 1000, $exists: 1 } }, { vooId: 1, litrosCombustivel: 1, _id: 0 });
