3513e3503f320e05681db275810a36999760e962
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-03-16 21:24:12 -0300

    desafio 22

--- challenges/desafio21.js
+++ challenges/desafio21.js
@@ -1,5 +1,14 @@
 db.voos.findOne({
-  litrosCombustivel: {
-    $gte: 1000,
-  },
+  $and: [
+    { $or: [
+      { "empresa.nome": "DELTA AIRLINES" },
+      { "empresa.nome": "AMERICAN AIRLINES" },
+    ] },
+    {
+      $and: [
+        { "aeroportoOrigem.sigla": "SBGR" },
+        { "aeroportoDestino.sigla": "KJFK" },
+      ],
+    },
+  ],
 }, { vooId: true, _id: false });
