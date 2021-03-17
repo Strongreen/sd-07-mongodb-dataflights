5dd06186ad575e539245ea26c846c2c8bc48edab
laurods <itrafor@gmail.com>
2021-03-16 21:00:33 -0300

    desafio 22 e 23

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,1 +1,1 @@
-db.voos.find({ $and: [{ "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] } }, { "aeroportoOrigem.sigla": "SBGR" }, { "aeroportoDestino.sigla": "kjfk"} ] }, { vooId: 1, _id: 0 });
+db.voos.findOne({ $and: [{ "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] } }, { "aeroportoOrigem.sigla": "SBGR" }, { "aeroportoDestino.sigla": "kjfk" }] }, { vooId: 1, _id: 0 });
