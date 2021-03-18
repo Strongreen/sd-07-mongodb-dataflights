12fce166815abac9cffce0ab8612240933058429
Marcio <marcio@unknown6432a883dae2.localdomain>
2021-03-17 21:36:30 -0300

    refat 23

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,8 +1,9 @@
 db.voos.find(
   {
-    litrosCombustivel: { $exists: true },
-    // eslint-disable-next-line no-dupe-keys
-    litrosCombustivel: { $lt: 1000 },
+    $and: [
+      { litrosCombustivel: { $exists: true } },
+      { litrosCombustivel: { $lt: 1000 } },
+    ],
   },
   { vooId: 1, litrosCombustivel: 1, _id: 0 },
 ).limit(1);
