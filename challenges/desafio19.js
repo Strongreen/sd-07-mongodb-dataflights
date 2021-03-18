9fa763578288d198f34d2785bcc00e3650cf631c
Arthur de Sa <arthurdesa94@gmail.com>
2021-03-17 21:07:05 -0300

    adicionando ate o 23, vqv

---
+++ challenges/desafio19.js
@@ -0,0 +1,3 @@
+db.voos.findOne(
+  { litrosCombustivel: { $exists: 1 } }, { vooId: 1, _id: 0 }
+);
---
+++ challenges/desafio21.js
@@ -0,0 +1,3 @@
+db.voos.findOne(
+  { litrosCombustivel: { $gt: 1000 } }, { vooId: 1, _id: 0 }
+);
