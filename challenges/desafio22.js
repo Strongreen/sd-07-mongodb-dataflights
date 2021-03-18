3795e662ed5f8acd5cc28e0a4255984e6b3067fb
julianafatsil <julianafatsil@gmail.com>
2021-03-18 13:55:13 -0300

    Desafio 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,13 @@
+db.voos.find(
+  {
+    "empresa.nome": {
+      $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"]
+    },
+    "aeroportoOrigem.sigla": "SBGR",
+    "aeroportoDestino": "KJFK",
+  },
+  {
+    vooId: 1,
+    _id: 0,
+  },
+).limit(1);
