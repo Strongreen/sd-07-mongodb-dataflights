b9d978b214573bf42d4af9b151125c1228fd2687
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-03-19 15:56:33 -0300

    Exercise 25 - done-2

--- challenges/desafio25.js
+++ challenges/desafio25.js
@@ -1,1 +1,3 @@
-db.voos.deleteMany({ and: [{ litrosCombustivel: { $lt: 400 } }, { "empresa.nome": "AZUL" }] });
+db.voos.deleteMany({
+  $and: [{ litrosCombustivel: { $lt: 400 } }, { "empresa.nome": "AZUL" }],
+});
