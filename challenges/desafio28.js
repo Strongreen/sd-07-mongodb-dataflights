d4aae18551a729ce87c278c1df079e79022f3331
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2021-03-20 16:56:06 -0300

    Commit  babado

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,3 +1,3 @@
 db.resumoVoos.insertOne({ empresa: "LATAM AIRLINES BRASIL", totalVoosDomesticos: db.voos.count({ "empresa.nome": "LATAM AIRLINES BRASIL", natureza: "Doméstica" }) });
 
-db.resumoVoos.findOne({ empresa: "LATAM AIRLINES BRASIL" }, { _id: 0 });
+db.resumoVoos.findOne({ empresa: "LATAM AIRLINES BRASIL" }, { empresa: 1, totalVoosDomesticos: 1, _id: 0 });
