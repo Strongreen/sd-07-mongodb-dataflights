307aed96a16d516e53d58d81330ac14b9ba9eb14
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2021-03-20 14:44:13 -0300

    req22

---
+++ challenges/desafio22.js
@@ -0,0 +1,1 @@
+db.voos.find({ $or: [{ "empresa.nome": "DELTA AIRLINES" }, { "empresa.nome": "AMERICAN AIRLINES" }] }, { "aeroportoOrigem.sigla": "SBGR", "aeroportoOrigem.sigla": "KJFK" }, { vooId: 1, _id: 0 }).limit(1);
