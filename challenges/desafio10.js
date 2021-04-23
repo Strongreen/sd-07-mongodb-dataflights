1bc4e53d62b62b6880b89680fa01fbca976a003b
André Ferreira <andreferreirap1@gmail.com>
2021-04-23 15:09:33 -0300

    Desafios 5 e 6 | Trocando 0 e 1 por false e true

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,12 +1,12 @@
 db.voos.find(
   { $and: [{ "empresa.nome": "GOL" }, { ano: 2017 }] },
   {
-    _id: 0,
-    vooId: 1,
-    "empresa.nome": 1,
-    "aeroportoOrigem.nome": 1,
-    "aeroportoDestino.nome": 1,
-    mes: 1,
-    ano: 1,
+    _id: false,
+    vooId: true,
+    "empresa.nome": true,
+    "aeroportoOrigem.nome": true,
+    "aeroportoDestino.nome": true,
+    mes: true,
+    ano: true,
   },
 ).limit(10);
