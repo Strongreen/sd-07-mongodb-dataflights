0eb550d11e49e6e05b16e778b73249e9d7ae6a2d
André Ferreira <andreferreirap1@gmail.com>
2021-04-24 20:14:46 -0300

    Desafio 27 | eslint

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,5 +1,5 @@
 db.resumoVoos.insertOne({ empresa: "PASSAREDO", totalVoosDomesticos: 4053 });
 db.resumoVoos.find(
   { empresa: "PASSAREDO" },
-  { _id: false, empresa: true, totalVoosDomesticos: true }
+  { _id: false, empresa: true, totalVoosDomesticos: true },
 ).limit(1);
