8652c0f785841879536352cd440dec45edfefaeb
André Ferreira <andreferreirap1@gmail.com>
2021-04-23 23:05:42 -0300

    Desafio 22 | limit(1) | eslint 2

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,8 +1,10 @@
-db.voos.find({
-  and: [
-    { "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] } },
-    { "aeroportoOrigem.sigla": "SBGR" },
-    { "aeroportoDestino.sigla": "KJFK" },
-  ]},
-  { _id: false, vooId: true }
+db.voos.find(
+  {
+    and: [
+      { "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] } },
+      { "aeroportoOrigem.sigla": "SBGR" },
+      { "aeroportoDestino.sigla": "KJFK" },
+    ],
+  },
+  { _id: false, vooId: true },
 ).limit(1);
