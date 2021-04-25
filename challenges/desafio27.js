419a5750d12612c2c52ca65900fc21f6158383d1
André Ferreira <andreferreirap1@gmail.com>
2021-04-25 00:18:49 -0300

    Desafio 27 | Avaliador

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -2,4 +2,4 @@ db.resumoVoos.insertOne({ empresa: "PASSAREDO", totalVoosDomesticos: 4053 });
 db.resumoVoos.find(
   { empresa: "PASSAREDO" },
   { _id: false, empresa: true, totalVoosDomesticos: true },
-).limit(1); //
+).limit(1);
