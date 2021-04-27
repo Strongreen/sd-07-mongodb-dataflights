8c7ab0e8c2a90adb6d819a5ff9a1bc415aba6b19
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2021-04-26 21:26:05 -0300

    resto dos desafios

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,8 +1,12 @@
 db.voos.findOne(
   {
-    $or: [
-      { "empresa.nome": { $eq: "DELTA AIRLINES" } },
-      { "empresa.nome": { $eq: "AMERICAN AIRLINES" } },
+    $and: [
+      {
+        $or: [
+          { "empresa.nome": { $eq: "DELTA AIRLINES" } },
+          { "empresa.nome": { $eq: "AMERICAN AIRLINES" } },
+        ],
+      },
       { "aeroportoOrigem.sigla": { $eq: "SBGR" } },
       { "aeroportoDestino.sigla": { $eq: "KJFK" } },
     ],
